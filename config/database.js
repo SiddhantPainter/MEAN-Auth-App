49f05b1871f853a8c36575842e983f2f3bc05529
siddhantpainter <siddhantpainter1@gmail.com>
2017-07-11 21:55:34 -0700

    linked db 3

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb:sid:sid@ds153752.mlab.com:53752/meanauthapp',
+  database: 'mongodb://siddhant:siddhant@ds153752.mlab.com:53752/meanauthapp',
   secret: 'yoursecret'
 }
